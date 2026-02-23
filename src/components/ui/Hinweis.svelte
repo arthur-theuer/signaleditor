3365baf91b405d0078aaecaa949bf29cd8ec8e40
Arthur Theuer <arthur.theuer@outlook.com>
2026-02-23 19:15:10 +0000

    Hinweis: simple card-gap spacing below button
    Co-authored-by: Ona <no-reply@ona.com>

--- src/components/ui/Hinweis.svelte
+++ src/components/ui/Hinweis.svelte
@@ -8,7 +8,7 @@
   .hinweis {
     display: none;
     position: absolute;
-    top: calc(100% + var(--spacing-cell) - var(--spacing-unit) / 4 + 0.5px);
+    top: calc(100% + var(--spacing-card));
     left: 50%;
     transform: translateX(-50%);
     height: calc(var(--spacing-unit) / 2);
